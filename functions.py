1d4d7c68a51a2f18248a87a39d2b5dca7f4b6fab
Daniel Berenfield <dberenfield@gmail.com>
2023-12-17 21:47:36 -0500

    changes

--- functions.py
+++ functions.py
@@ -11,13 +11,12 @@ def max_num(x, y, z):
     print(max_num)
 
 
-    mult_list=[3,4,5,4,7]
-total = 1
-for i in mult_list:
-	total *= i
-
-print(total)
-
+  def mult_list(num):
+a = 1
+for i in num:
+	a = a * 1
+    return a
+    print(mul[5,2,3])
 
 def rev_string(s):
     str = ""
@@ -32,3 +31,14 @@ def num_within(n):
         print("The number is in the range" % str(n))
     else:
         print("The number is outside the given range.")
+
+
+
+def pascal(n):
+    trow = [1]
+     y = [0]
+    for x in range(max(n, 0)):
+        print(trow)
+        trow = [l + r for l, r in zip(trow + y, y + trow)]
+      return n >= 1
+pascal(6) 
